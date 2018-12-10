b162b130f49d4253444becc00b27752ed676cae3
bmilius-nmdp <bmilius@nmdp.org>
2018-12-09 21:15:03 -0600

    changed settings

--- demo-settings.js
+++ demo-settings.js
@@ -10,15 +10,15 @@
 // };
 
 
-// var demo = {
-//     serviceUrl: "https://r3.smarthealthit.org",
-//     patientId: "7b697322-3607-46cb-a240-c081bccba2e5"
-// };
-
 var demo = {
-    serviceUrl: "https://apporchard.epic.com/interconnect-aocurprd-username/api/FHIR/STU3",
-    patientId: "eyQ4e4Hfq5yH.4vWYwqI9PA3"
-}
+    serviceUrl: "https://r3.smarthealthit.org",
+    patientId: "7b697322-3607-46cb-a240-c081bccba2e5"
+};
+
+// var demo = {
+//    serviceUrl: "https://apporchard.epic.com/interconnect-aocurprd-username/api/FHIR/STU3",
+//    patientId: "eyQ4e4Hfq5yH.4vWYwqI9PA3"
+// }
 
 /*
 var demo = {
